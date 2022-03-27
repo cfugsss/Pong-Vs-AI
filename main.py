d6715666f16da72138c9564f19b93de83ab59620
Chris <ceefmanagement@gmail.com>
2022-03-26 21:25:00 -0400

    main loop start

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import pygame
+import pygments
 import ai
 import player
+
+DISPLAY_W, DISPLAY_H = 700, 500
+WIN = pygame.display.set_mode((DISPLAY_W, DISPLAY_H))
+pygame.display.set_caption("Pong VS AI")
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event == event.QUIT:
+                run = False
+                
